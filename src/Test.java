8c4ece103ad90edc99b2ac432dfa99c493e3109d
PanxyCris <739312347@qq.com>
2019-08-05 17:38:45 +0800

    Fix

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,11 @@
  * Created by Panxy on 2019/8/5
  */
 public class Test {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        int c  = add(1,2);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
